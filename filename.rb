007de06e60f637e7ad8fd1da32ed860342725e57
Kelsey Thompson <kelsthompson2@gmail.com>
2021-07-13 11:41:53 -0400

    initial commit

---
+++ filename.rb
@@ -0,0 +1,22 @@
+class Ship
+  attr_reader :name, :length, :health, :is_sunk
+
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @health = @length
+    @is_sunk = false
+  end
+
+  def sunk?
+    if @health > 0
+      @is_sunk
+    else
+      @is_sunk = true
+    end
+  end
+
+  def hit
+    @health -= 1
+  end
+end
